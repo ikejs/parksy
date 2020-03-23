54a4a1fc0e81a25c0aad9f9eb2a90fdd120b1133
Ike <ikescode@gmail.com>
2020-03-22 23:17:51 -0500

    implement MomentJS and datepicker, add table for setting event dates/times

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -4,7 +4,8 @@ new google.maps.places.Autocomplete(
     types: ['geocode']
 });
 
-$(document).ready( function() {
+
+$(document).ready(() => {
 
     // Image Upload
     $(document).on('change', '.btn-file :file', function() {
@@ -12,7 +13,6 @@ $(document).ready( function() {
         label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
         input.trigger('fileselect', [label]);
     });
-
     $('.btn-file :file').on('fileselect', function(event, label) {
         let input = $(this).parents('.input-group').find(':text'),
             log = label;
@@ -20,7 +20,6 @@ $(document).ready( function() {
             input.val(log);
         }
     });
-
     const readURL = (input) => {
         if (input.files && input.files[0]) {
             const reader = new FileReader();
@@ -30,21 +29,30 @@ $(document).ready( function() {
             reader.readAsDataURL(input.files[0]);
         }
     }
-
     $("#imgInp").change(function(){
         readURL(this);
         $("#img-upload").removeClass('hidden');
         $("#uploadBtn").removeClass('btn-info');
         $("#uploadBtn").addClass('btn-warning');
         $("#uploadBtnText").html(`<span class="fa fa-pencil-alt"></span> Change`);
-    }); 	
+    });
+    
+
+    // lot type event/everyday switcher
+    $("input[name=parkingType]").on("change", function() {
+        const switchTo = $(this).val();
+        if(switchTo === 'eventParking') {
+            $("#eventDates").removeClass('hidden');
+            $("#everydayDates").addClass('hidden');
+        } else {
+            $("#eventDates").addClass('hidden');
+            $("#everydayDates").removeClass('hidden');
+        }
+    });
 
 
-
-    // all/each editor
+    // edit all/each spaces switcher
     $("input[name=allEachSelector]").on("change", function() {
-
-
         const switchTo = $(this).val();
         if(switchTo === 'all') {
             $("#editAllSpaces").removeClass('hidden');
@@ -54,7 +62,6 @@ $(document).ready( function() {
             $("#editEachSpace").removeClass('hidden');
         }
    });
-
 
 });
 
